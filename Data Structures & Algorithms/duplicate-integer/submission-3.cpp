78461d351d8e6aa7942bb13bcbe91e5259e88b6c
deepdasdev <dasd17933@gmail.com>
2026-05-23 13:18:51 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,22 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> seen;
+
+        for(int i = 0 ; i < n; i++){
+        
+           if( seen.count(nums[i])) {
+            return true;
+           }
+           seen.insert(nums[i]);
+           
+        }
+        return false;
+    }
+};
