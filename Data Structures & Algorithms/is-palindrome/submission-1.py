7aec39e02c0ed2b7995aa68dac32e2524d4855d4
DEEP DAS <dasd17933@gmail.com>
2026-08-25 16:53:29 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.split(" ")
+        s = "".join(s)
+        n = len(s)
+        i = 0
+        j = n-1
+        k = [",",".","?","!","'"]
+        while i <= j:
+            if s[i] in k:
+                i += 1
+            if s[j] in k:
+                j -= 1
+            if s[i].lower() != s[j].lower() :
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True                    
